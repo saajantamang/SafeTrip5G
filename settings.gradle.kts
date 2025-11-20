6a8a93690c2c697a965266a0beb109c310decf4d
Saajan Tamang <82790275+saajantamang@users.noreply.github.com>
2025-11-20 19:04:27 +0000

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SafeTrip 5G"
+include(":app")
+ 
